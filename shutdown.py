5dfe80f1a6f063d0c91ab64fd113f61121ac037a
Hendricks, Brent Andrew <brent.hendricks@p5software.com>
2021-07-04 19:50:29 -0400

    Initial commit

---
+++ shutdown.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+
+def execute(topic, payload):
+
+    returnValue = {}
+
+    result = subprocess.run(['shutdown', stderr=subprocess.PIPE)
+
+    if result.returncode != 0:
+
+        returnValue['message'] = result.stderr.decode('ascii').strip()
+
+        raise Exception(result.stderr.decode('ascii').strip())
+
+    returnValue['message'] = "OK"
+
+    return returnValue
